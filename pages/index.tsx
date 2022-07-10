b5f7ebfa3eee3c3d969faf0a280db127235a7cee
CryptoAngel1024 <cryptoangel1024@gmail.com>
2022-07-11 03:33:55 +0800

    completed

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import CoinbaseWalletCard from '../components/connectorCards/CoinbaseWalletCard'
+import GnosisSafeCard from '../components/connectorCards/GnosisSafeCard'
+import MetaMaskCard from '../components/connectorCards/MetaMaskCard'
+import NetworkCard from '../components/connectorCards/NetworkCard'
+import WalletConnectCard from '../components/connectorCards/WalletConnectCard'
+import ProviderExample from '../components/ProviderExample'
+
+export default function Home() {
+  return (
+    <>
+      <ProviderExample />
+        <MetaMaskCard />
+        <WalletConnectCard />
+        <CoinbaseWalletCard />
+        <NetworkCard />
+    </>
+  )
+}
